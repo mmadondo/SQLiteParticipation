4c33c7d3e908970a9e5ca530b3b2b8fee205ba2a
Malvern <mmadondo@users.noreply.github.com>
2017-04-02 12:54:59 -0500

    Update Comment.java

--- app/src/main/java/commmadondo/github/sqliteparticipation/Comment.java
+++ app/src/main/java/commmadondo/github/sqliteparticipation/Comment.java
@@ -6,28 +6,37 @@ package commmadondo.github.sqliteparticipation;
  * Contains data to be used in database and shown in the UI
  */
 
-public class Comment {
+public class Comment{
     private long id;
     private String comment;
 
+    //retrieves and returns comment id 
     public long getId() {
         return id;
     }
 
+    //sets the id
     public void setId(long id) {
         this.id = id;
     }
 
+    //retrieves and returns comment
     public String getComment() {
         return comment;
     }
 
+    //sets the comment to a string
     public void setComment(String comment) {
         this.comment = comment;
     }
 
-    // Will be used by the ArrayAdapter in the ListView
+   
     @Override
+
+    /**
+    *used by the ArrayAdapter in the ListView
+    *returns comment
+    */
     public String toString() {
         return comment;
     }
